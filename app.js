70745dff72486d52bca8bcd460412b06eed6e3e1
Luther <luther@autostructure.io>
2019-08-19 10:48:16 -0400

    Test change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-    res.send('Sample Node.js App WHICH IS NOW AWESOME');
+    res.send('Sample Node.js App WHICH IS NOW AWESOME AND COOOL');
 });
 
 app.listen(port, function () {
